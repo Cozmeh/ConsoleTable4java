0ce6f8d7937999f11bbe5a2881585d48d818a9f1
Cozmeh <hemsainath15@gmail.com>
2023-03-25 08:46:37 +0530

    usage

--- UsageTest.java
+++ UsageTest.java
@@ -3,20 +3,26 @@ package ct4j;
 class UsageTest 
 {
 
-    public static void main(String[] args) throws ElementSizeException, MultipleHeaderException,DuplicateHeaderException{
-
+    public static void main(String[] args) throws Exception {
+        
         ct4j table = new ct4j();
-
-        String[] header = {"first","second","third","last"};
-        String[] row = {"shortword","superverylongword","normalword","lol"};
         
-        table.setHeader(header);
-        table.addRow(row);
-        table.addRow(row);
-        table.addRow(row);
-        table.addRow(row);
+        // Set table headers
+        table.setHeader("Name", "Age", "City");
         
+        // Add data/rows to the table
+        table.addRow("Krishna", "24", "Dwaraka");
+        table.addRow("Arjuna", "21", "Hastinapur");
+        
+        // Customize the table
+        table.setHorizontalSeparator('-');
+        table.setVerticalSeparator('|');
+        table.setCornerJoint('+');
+        table.setUppercaseHeaders(true);
+        
+        // Print the table
         table.printTable();
         
     }
 }
+
